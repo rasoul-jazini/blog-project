284f79dc290eaec0daafc6723d98d5157647ab25
Rasool97 <raybodjazini97@gmail.com>
2022-08-17 12:43:31 +0430

    Add mui and graphql to project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import "./styles/index.css";
+import "./styles/fonts.css";
+import { ThemeProvider } from "@emotion/react";
+import theme from "./mui/them";
+
+const client = new ApolloClient({
+  uri: "https://api-us-west-2.hygraph.com/v2/cl6w9fya446p101uq845zc9re/master",
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <ApolloProvider client={client}>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </ApolloProvider>
+);
