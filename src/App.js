e0e30a29697cc313a447eb69a9c24e4563ce4344
Rasool97 <raybodjazini97@gmail.com>
2022-08-20 00:13:27 +0430

    Fixed scrolling bug

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import HomePage from "./components/home/HomePage";
 import BlogsPage from "./components/blog/BlogsPage";
 import AuthorsPage from "./components/author/AuthorsPage";
 import { Toaster } from "react-hot-toast";
+import ScrollToTop from "./components/shared/ScrollToTop";
 
 function App() {
   return (
     <Layout>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/blogs/:slug" element={<BlogsPage />} />
